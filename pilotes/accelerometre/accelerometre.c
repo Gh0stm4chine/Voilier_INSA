301de3e988ff46118e5628a0efe94ce96fcea345
timotheeficat <ficat@insat.insa-toulouse.fr>
2016-10-19 11:37:20 +0200

    MAJ accelerometre

--- pilotes/accelerometre/accelerometre.c
+++ pilotes/accelerometre/accelerometre.c
@@ -1,1 +1,16 @@
-﻿#include "accelerometre.h"
+#include "accelerometre.h"
+#include "stm32f10x.h"
+
+void init_accelero() {
+	// Enable clock sur GPIOC
+	(RCC->APB2ENR)|= RCC_APB2ENR_IOPCEN;
+	// Analog input pour PC0 et PC1
+	GPIOC->CRL &= ~(0xF);
+	GPIOC->CRL &= ~(0xF<<4);
+	
+}
+
+float get_angle_accelero() {
+	
+	return 0;
+}
--- pilotes/accelerometre/accelerometre.h
+++ pilotes/accelerometre/accelerometre.h
@@ -1,6 +1,15 @@
-﻿#ifndef ACCELERO_H
+#ifndef ACCELERO_H
 #define ACCELERO_H
 
+/**
+*	Permet d'initialiser l'acceleromètre.
+**/
+void init_accelero();
 
+/**
+* Permet de récupérer la valeur de l'inclinaison du bateau compris en 0 et 360 degrès.
+* Le bateau est parfaitement droit à 180 degrès.
+**/
+float get_angle_accelero();
 
 #endif
