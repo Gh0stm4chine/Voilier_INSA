cb1d00c76c7f42cd55ba823c261f37179f7b41c2
bsoso <sohun@hotmail.fr>
2016-10-24 13:14:13 +0200

    DCg global

--- pilotes/girouette/girouette.c
+++ pilotes/girouette/girouette.c
@@ -1,7 +1,7 @@
 #include "girouette.h"
 
-void init_timer_girouette (void){
-	(RCC->APB1ENR)|= RCC_APB1ENR_TIM3EN;
+void init_girouette(void) {
+	init_port_timer (TIM3);
 	//Select Encoder interface mode : TI1 edge 001 ? TI2 edge 010 ? both ? 011
 	TIM3->SMCR &= ~TIM_SMCR_SMS;
 	TIM3->SMCR |= TIM_SMCR_SMS_0; //Counting on TI1
@@ -10,10 +10,6 @@ void init_timer_girouette (void){
 				0: non-inverted: capture is done on a rising edge of IC1. When used as external */
 	TIM3->CR1 |= TIM_CR1_CEN;
 	//Select Nbre de pistes par tour
-	TIM3->ARR = 360;
-	
-}
-
-void init_girouette(void) {
-	init_timer_girouette();
+	TIM3->ARR = 359; // ou un multiple de 360 ? dans ce cas là, T = 5 us :/ 
+	TIM3->PSC = 0;
 }
--- pilotes/girouette/girouette.h
+++ pilotes/girouette/girouette.h
@@ -1,10 +1,9 @@
 #ifndef GIROUETTE_H
 #define GIROUETTE_H
 #include "stm32f10x.h"
+#include "timer_1234.h"
 
-/**Fonction permettant l'initialisation du timer de la girouette **/
-void init_timer_girouette (void);
-
+/**Fonction permettant l'initialisation de la girouette **/
 void init_girouette(void);
 
 
